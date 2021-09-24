92faba8efd0af7883f1528afc3435a45bdbb6c15
Mikhail <mumeu222@mail.ru>
2021-09-24 23:14:05 +0300

    change default mux to gorilla mux

--- server.go
+++ server.go
@@ -4,9 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"text/template"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // type User struct {
@@ -25,7 +30,7 @@ var (
 
 type StatusLogedInJSON struct {
 	Status string `json:"status"` // status 400 200
-	// body
+	// Body   interface{}
 }
 
 type LoginUser struct {
@@ -136,26 +141,65 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func main() {
 	users["mumeu222@mail.ru"] = "VBif222!"
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", homePageHandler)
-	mux.HandleFunc("/api/v1/login", loginHandler)
-	mux.HandleFunc("/logout", logoutHandler)
-	staticHandler := http.StripPrefix(
-		"/data/",
-		http.FileServer(http.Dir("./static")),
-	)
-	mux.Handle("/data/", staticHandler)
+	mux := mux.NewRouter()
 
-	server := http.Server{
-		Addr:         ":8080",
+	mux.HandleFunc("/api/v1/login", loginHandler)
+	// mux.HandleFunc("/logout", logoutHandler)
+
+	spa := spaHandler{staticPath: "static", indexPath: "index.html"}
+	mux.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
 		Handler:      mux,
-		ReadTimeout:  http.DefaultClient.Timeout,
+		Addr:         "127.0.0.1:8080",
 		WriteTimeout: http.DefaultClient.Timeout,
+		ReadTimeout:  http.DefaultClient.Timeout,
 	}
 
-	fmt.Println("starting server at :8080")
-	server.ListenAndServe()
+	log.Fatal(srv.ListenAndServe())
 }
