53e1702b91ee24f98eda3f74078e5bc046f04111
perlinleo <perlinleo@github.com>
2021-09-30 13:04:04 +0000

    check on server

--- server.go
+++ server.go
@@ -11,7 +11,7 @@ import (
 	"strconv"
 	"time"
 
-	_ "./docs"
+	_ "server/docs"
 
 	"github.com/gorilla/mux"
 	httpSwagger "github.com/swaggo/http-swagger"
@@ -417,20 +417,12 @@ func init() {
 	}
 }
 
-// @title Swagger Example API
+// @title Drip API
 // @version 1.0
-// @description This is a sample server Petstore server.
+// @description API for Drip.
 // @termsOfService http://swagger.io/terms/
 
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
 // @host petstore.swagger.io
-// @BasePath /v2
 func main() {
 	env := &Env{
 		db:        db, // NewMockDB()
