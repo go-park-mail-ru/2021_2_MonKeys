7425d4929a1f2043028f55287a1c7877f0b8430c
Ilyagu <inagdimaev@mail.ru>
2021-11-07 01:39:29 +0300

    csrf

--- internal/dripapp/user/delivery/user_routing.go
+++ internal/dripapp/user/delivery/user_routing.go
@@ -22,34 +22,6 @@ func SetRouting(loggger logger.Logger, router *mux.Router, us models.UserUsecase
 		SessionUcase: su,
 	}
 
-	// router.HandleFunc("/api/v1/session",
-	// 	permissions.SetCSRF(sessionHandler.LoginHandler)).Methods("POST", "OPTIONS")
-	// router.HandleFunc("/api/v1/session",
-	// 	permissions.CheckCSRF(permissions.CheckAuthenticated(sessionHandler.LogoutHandler))).Methods("DELETE", "OPTIONS")
-
-	// router.HandleFunc("/api/v1/profile",
-	// 	permissions.SetCSRF(permissions.CheckAuthenticated(userHandler.CurrentUser))).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/v1/profile",
-	// 	permissions.CheckCSRF(permissions.CheckAuthenticated(userHandler.EditProfileHandler))).Methods("PUT", "OPTIONS")
-	// router.HandleFunc("/api/v1/profile",
-	// 	permissions.SetCSRF(userHandler.SignupHandler)).Methods("POST", "OPTIONS")
-
-	// router.HandleFunc("/api/v1/profile/photo",
-	// 	permissions.CheckAuthenticated(userHandler.UploadPhoto)).Methods("POST", "OPTIONS")
-	// router.HandleFunc("/api/v1/profile/photo",
-	// 	permissions.CheckAuthenticated(userHandler.DeletePhoto)).Methods("DELETE", "OPTIONS")
-
-	// router.HandleFunc("/api/v1/user/cards",
-	// 	permissions.SetCSRF(permissions.CheckAuthenticated(userHandler.NextUserHandler))).Methods("GET", "OPTIONS")
-
-	// router.HandleFunc("/api/v1/matches",
-	// 	permissions.SetCSRF(permissions.CheckAuthenticated(userHandler.MatchesHandler))).Methods("GET", "OPTIONS")
-
-	// router.HandleFunc("/api/v1/tags",
-	// 	permissions.SetCSRF(permissions.CheckAuthenticated(userHandler.GetAllTags))).Methods("GET", "OPTIONS")
-
-	// router.PathPrefix("/api/documentation/").Handler(httpSwagger.WrapHandler)
-
 	router.HandleFunc("/api/v1/session", permissions.SetCSRF(sessionHandler.LoginHandler)).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/v1/session", permissions.CheckAuthenticated(sessionHandler.LogoutHandler)).Methods("DELETE", "OPTIONS")
 
