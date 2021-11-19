6ad594de9b26c59be3e56d9681837c08e7e534a0
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-11-19 23:43:16 +0300

    auto create default profile

--- cmd/dripapp/main.go
+++ cmd/dripapp/main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"context"
 	"dripapp/configs"
 	"dripapp/internal/dripapp/file"
 	_fileDelivery "dripapp/internal/dripapp/file/delivery"
 	"dripapp/internal/dripapp/middleware"
+	"dripapp/internal/dripapp/models"
 	_sessionDelivery "dripapp/internal/dripapp/session/delivery"
 	_sessionRepo "dripapp/internal/dripapp/session/repository"
 	_sessionUcase "dripapp/internal/dripapp/session/usecase"
 	_userDelivery "dripapp/internal/dripapp/user/delivery"
 	_userRepo "dripapp/internal/dripapp/user/repository"
 	_userUsecase "dripapp/internal/dripapp/user/usecase"
+	"dripapp/internal/pkg/hasher"
 	"dripapp/internal/pkg/logger"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	_ "dripapp/docs"
 
 	"github.com/gorilla/mux"
 )
+
+func startRepo(r models.UserRepository, f models.FileRepository)  {
+	time.Sleep(time.Second)
+	loginData := models.LoginUser{
+		Email: "qwe@qwe",
+		Password: hasher.HashAndSalt(nil, "qweQWE12"),
+	}
+	user, err := r.CreateUser(context.Background(), loginData)
+	fmt.Println("CreateUser: ", err)
+
+	err = f.CreateFoldersForNewUser(user)
+	fmt.Println("CreateFoldersForNewUser: ", err)
+
+	err = user.FillProfile(models.User{
+		Name: "Vladimir",
+		Date: "2004-01-02",
+		Description: "Description Description 123",
+		Imgs: []string{"wsx.webp"},
+	})
+	fmt.Println("FillProfile: ", err)
+
+	_, _ = r.UpdateUser(context.Background(), user)
+}
 
 func main() {
 	// logfile
@@ -82,6 +110,7 @@ func main() {
 
 	log.Printf("STD starting server at %s\n", srv.Addr)
 
+	startRepo(userRepo, fileManager)
 	// for local
 	log.Fatal(srv.ListenAndServe())
 	// for deploy
