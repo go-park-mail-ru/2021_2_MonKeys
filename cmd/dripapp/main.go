a2177e520d5abca703ae6ba3783a7bb40ea966cb
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-11-24 12:50:26 +0300

    create default user

--- cmd/dripapp/main.go
+++ cmd/dripapp/main.go
@@ -1,77 +1,74 @@
 package main
 
 import (
+	"context"
 	"dripapp/configs"
 	"dripapp/internal/dripapp/file"
 	_fileDelivery "dripapp/internal/dripapp/file/delivery"
 	"dripapp/internal/dripapp/middleware"
+	"dripapp/internal/dripapp/models"
 	_sessionDelivery "dripapp/internal/dripapp/session/delivery"
 	_sessionRepo "dripapp/internal/dripapp/session/repository"
 	_sessionUcase "dripapp/internal/dripapp/session/usecase"
 	_userDelivery "dripapp/internal/dripapp/user/delivery"
 	_userRepo "dripapp/internal/dripapp/user/repository"
 	_userUsecase "dripapp/internal/dripapp/user/usecase"
+	"dripapp/internal/pkg/hasher"
 	"dripapp/internal/pkg/logger"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	_ "dripapp/docs"
 
 	"github.com/gorilla/mux"
 )
 
-// func createUser(r models.UserRepository, f models.FileRepository, name string) uint64 {
-// 	loginData := models.LoginUser{
-// 		Email:    name + "@mail.ru",
-// 		Password: hasher.HashAndSalt(nil, "qweQWE12"),
-// 	}
-// 	user, err := r.CreateUser(context.Background(), loginData)
-// 	fmt.Println("CreateUser: ", err)
+func createUser(r models.UserRepository, f models.FileRepository, name string) uint64 {
+	loginData := models.LoginUser{
+		Email:    name + "@mail.ru",
+		Password: hasher.HashAndSalt(nil, "qweQWE12"),
+	}
+	user, err := r.CreateUser(context.Background(), loginData)
+	fmt.Println("CreateUser: ", err)
 
-// 	err = f.CreateFoldersForNewUser(user)
-// 	fmt.Println("CreateFoldersForNewUser: ", err)
+	err = f.CreateFoldersForNewUser(user)
+	fmt.Println("CreateFoldersForNewUser: ", err)
 
-// 	userData := models.User{
-// 		ID:          user.ID,
-// 		Email:       user.Email,
-// 		Password:    user.Password,
-// 		Name:        name,
-// 		Gender:      "male",
-// 		FromAge:     18,
-// 		ToAge:       100,
-// 		Date:        "2004-01-02",
-// 		Description: "Всем привет меня зовут" + name,
-// 		Imgs:        []string{"wsx.webp"},
-// 	}
-// 	fmt.Println("FillProfile: ", err)
+	userData := models.User{
+		ID:          user.ID,
+		Email:       user.Email,
+		Password:    user.Password,
+		Name:        name,
+		Gender:      "male",
+		FromAge:     18,
+		ToAge:       100,
+		Date:        "2004-01-02",
+		Description: "Всем привет меня зовут" + name,
+		Imgs:        []string{"wsx.webp"},
+	}
+	fmt.Println("FillProfile: ", err)
 
-// 	_, _ = r.UpdateUser(context.Background(), userData)
+	_, _ = r.UpdateUser(context.Background(), userData)
 
-// 	return user.ID
-// }
+	return user.ID
+}
 
-// func startRepo(r models.UserRepository, cr models.ChatRepository, f models.FileRepository) {
-// 	time.Sleep(3 * time.Second)
+func startRepo(r models.UserRepository, f models.FileRepository) {
+	time.Sleep(3 * time.Second)
 
-// 	userID1 := createUser(r, f, "Ilyagu")
-// 	userID2 := createUser(r, f, "Vova")
-// 	userID3 := createUser(r, f, "Misha")
+	userID1 := createUser(r, f, "Ilyagu")
+	userID2 := createUser(r, f, "Vova")
+	_ = createUser(r, f, "Misha")
 
-// 	ctx := context.Background()
-// 	r.AddReaction(ctx, userID1, userID2, 1)
-// 	//r.AddReaction(ctx, userID2, userID1, 1)
+	ctx := context.Background()
+	r.AddReaction(ctx, userID1, userID2, 1)
+	//r.AddReaction(ctx, userID2, userID1, 1)
 
-// 	// Message
-// 	//_, err := cr.SaveMessage(userID1, userID2, "")
-// 	//_, err = cr.SaveMessage(userID2, userID1, "")
-// 	//_, err = cr.SaveMessage(userID1, userID2, "AAAAAAAAA!")
-
-// 	_, err := cr.SaveMessage(userID1, userID3, "")
-// 	_, err = cr.SaveMessage(userID3, userID1, "")
-// 	_, err = cr.SaveMessage(userID1, userID3, "первое сообщение!")
-// 	fmt.Println("SendMessage: ", err)
-// }
+	//fmt.Println("SendMessage: ", err)
+}
 
 func main() {
 	// logfile
@@ -117,20 +114,10 @@ func main() {
 		timeoutContext,
 	)
 
-	// chat
-	// hub := models.NewHub()
-	// go hub.Run()
-	// chatRepo, err := _chatRepo.NewPostgresChatRepository(configs.Postgres)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// chatUseCase := _chatUsecase.NewChatUseCase(chatRepo, hub, timeoutContext)
-
 	// delivery
 	_userDelivery.SetUserRouting(logger.DripLogger, router, userUCase, sessionUcase, userRepo)
 	_sessionDelivery.SetSessionRouting(logger.DripLogger, router, userUCase, sessionUcase)
 	_fileDelivery.SetFileRouting(router, *fileManager)
-	// _chatDelivery.SetChatRouting(logger.DripLogger, router, chatUseCase, userRepo)
 
 	// middleware
 	middleware.NewMiddleware(router, sm, logFile, logger.DripLogger)
@@ -144,7 +131,7 @@ func main() {
 
 	log.Printf("STD starting server at %s\n", srv.Addr)
 
-	// go startRepo(userRepo, chatRepo, fileManager)
+	go startRepo(userRepo, fileManager)
 	// for local
 	log.Fatal(srv.ListenAndServe())
 	// for deploy
